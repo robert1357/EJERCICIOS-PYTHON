625a9d467769b07a6451bcdd9664e286fd023542
robert1357 <roberth.gonzales@ucsp.edu.pe>
2024-09-11 22:17:12 -0500

    Add files via upload

---
+++ ejercicios python/ejer1.py
@@ -0,0 +1,4 @@
+peso = float(input("Introduce tu peso en kg: "))
+estatura = float(input("Introduce tu estatura en metros: "))
+imc = peso / estatura**2
+print(f"Tu índice de masa corporal es {imc}")
